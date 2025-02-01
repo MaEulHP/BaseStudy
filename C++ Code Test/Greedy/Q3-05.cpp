e6f374e7cfc08f716a408ee8dd4a5a9efaf20d2d
MaEulHP <boy123753@gmail.com>
2025-02-01 13:52:40 +0900

    실전 문제

--- C++ Code Test/Greedy/Q3-05.cpp
+++ C++ Code Test/Greedy/Q3-05.cpp
@@ -7,11 +7,14 @@ int main()
     int N, M;
     cin >> N >> M;
     vector<int> ball;
+
     for(int i=0, j; i<N; ++i){
         cin >> j;
         ball.push_back(j);
     }
+
     int Comb = 0;
+    
     for(int i=0; i<N-1; ++i){
         for(int j=i+1; j<N; ++j){
             if(ball.at(i) == ball.at(j)){
--- C++ Code Test/Implementation/Q4-02.cpp
+++ C++ Code Test/Implementation/Q4-02.cpp
@@ -1,3 +1,56 @@
 #include <iostream>
 
 using namespace std;
+
+int n, m, x, y, d;
+
+int dx[] {-1, 0, 1, 0};
+int dy[] {0, 1, 0, -1};
+
+void turn() {
+    --d;
+    if(d == -1) d = 3;
+}
+
+int main()
+{
+    cin >> n >> m;
+    cin >> x >> y >> d;
+
+    int visit[50][50];
+    int map[50][50];
+
+    visit[x][y] = 1;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            int k; cin >> k; map[i][j] = k;
+        }
+    }
+
+    int cnt = 0;
+    int turn_time = 0;
+    while(true){
+        turn();
+        int nextX = x + dx[d];
+        int nextY = y + dy[d];
+        if(visit[nextX][nextY] == 0 && map[nextX][nextY] == 0){
+            visit[nextX][nextY] = 1;
+            x = nextX; y = nextY;
+            ++cnt;
+            turn_time = 0;
+            continue;
+        }
+        else turn_time += 1;
+        if(turn_time == 4){
+            nextX = x - dx[d];
+            nextY = y - dy[d];
+            if(map[nextX][nextY] == 0){
+                x = nextX; y = nextY;
+            }
+            else break;
+            turn_time = 0;
+        }
+    }
+    cout << cnt;
+}
